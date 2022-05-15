56072648d2935e8a7e607f085158f86b83a8871d
Dezzex <dezzex@yandex.ru>
2022-05-15 21:20:11 +0300

    Create shootlogic, start creating enemyPrefabs

--- Spacewar/Assets/Scripts/PlayerController.cs
+++ Spacewar/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     private float Speed = 10.0f;
     private float _zBound = 7.0f;
+    public float bulletSpeed = 35.0f;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Movement();
+        Shooting();
     }
 
     private void Movement()
@@ -40,5 +43,14 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, _zBound);
         }
 
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject b = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            b.GetComponent<Rigidbody>().AddForce(Vector3.forward * bulletSpeed, ForceMode.Impulse);
+        }
     }
 }
